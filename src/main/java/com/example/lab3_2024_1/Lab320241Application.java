f3c2d9fc57bc79e68252b9638cc5315f3c05d27a
Hineill <hineill.cespedes@pucp.edu.pe>
2024-04-24 01:38:16 -0500

    Hola

---
+++ src/main/java/com/example/lab3_2024_1/Lab320241Application.java
@@ -0,0 +1,13 @@
+package com.example.lab3_2024_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab320241Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab320241Application.class, args);
+	}
+
+}
